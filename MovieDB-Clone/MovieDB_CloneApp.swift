76a8df10457c137bba84e8517eb57c0489e00b41
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-03-25 14:30:31 -0300

    Envio inicial dos arquivos

---
+++ MovieDB-Clone/MovieDB_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDB_CloneApp.swift
+//  MovieDB-Clone
+//
+//  Created by Joao Gabriel Dourado Cervo on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieDB_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
